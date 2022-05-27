b2c22e2e5e4237c1aed30f605029054e005d3a01
danilo cucharro <danicucharro@outlook.com>
2022-05-27 11:24:21 -0300

    tentando conexao com bd na nuvem da aws

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 
 // Conexao com o Banco de dados
-const sequelize = new Sequelize('projetopreaula', 'root', '@Bk_010354', {
-    host: "localhost",
+const sequelize = new Sequelize('BD_WKbody', 'root', 'Bk_010354', {
+    host: "wkbody-bd.cnnlvrvgqbrq.sa-east-1.rds.amazonaws.com",
     dialect: 'mysql',
     query:{raw:true}
 })
